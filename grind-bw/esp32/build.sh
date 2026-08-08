00ee7457b799126d4a9d1f1a000e848a687024a7
George A. <george.a13@gmail.com>
2026-08-08 09:43:09 +0300

    adding 'monitor' optional and makeing sure we always return to the original folder

--- grind-bw/esp32/build.sh
+++ grind-bw/esp32/build.sh
@@ -1,4 +1,17 @@
-cd firmware
-pio run -t clean
-pio run -t upload
+#!/usr/bin/env bash
+
+(
+  cd firmware || exit 1
+
+  pio run -t clean
+
+  targets=(-t upload)
+  if [[ "$1" == "-m" || "$1" == "--monitor" || "$1" == "monitor" ]]; then
+      targets+=(-t monitor)
+  fi
+
+  pio run "${targets[@]}"
+)
+
+# back in the original directory here, even if pio crashed
 
